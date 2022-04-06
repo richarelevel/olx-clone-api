da0aabdf9e34c1f1ac8df3b20c90490cd7dbc1f4
richasinhaa <richa.sinha0603@gmail.com>
2022-04-06 15:06:33 +0800

    Olx Clone API Template added.

---
+++ src/main/java/com/api/olxclone/OlxCloneApplication.java
@@ -0,0 +1,13 @@
+package com.api.olxclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OlxCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OlxCloneApplication.class, args);
+	}
+
+}
